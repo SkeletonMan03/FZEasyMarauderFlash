64624f3b6c108c0c059c99dd6026c53aef5db8cc
SkeletonMan03 <96273359+SkeletonMan03@users.noreply.github.com>
2022-12-11 17:49:09 -0600

    Put back a 5 second sleep after erasing

--- EasyInstall.py
+++ EasyInstall.py
@@ -129,6 +129,8 @@ def erase_esp32fw():
 	print("Erasing firmware...")
 	esptool.main(['-p', serialport, '-b', BR, '-c', chip, '--before', 'default_reset', '-a', 'no_reset', 'erase_region', '0x9000', '0x6000'])
 	print("Firmware erased!")
+	print("Waiting 5 seconds...")
+	time.sleep(5)
 	return
 
 def checkforesp32marauder():
